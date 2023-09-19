27910b3a4963efa73ee7460591a9af085380130c
Alexandre Regalado <alexandreregalado@hotmail.com>
2023-09-19 18:52:47 +0100

    tp0 ex5

---
+++ src/t0/mainAudio.py
@@ -0,0 +1,43 @@
+from scipy.io import wavfile
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def apresentarInfo(nomeFicheiro, fs, nrBitsQuant):
+	print('Informações do ficheiro')
+
+	print(f'Nome: {nomeFicheiro}')
+	print(f'Taxa de amostragem: {fs}')
+	print(f'Quantização: {nrBitsQuant}')
+
+def visualizacaoGrafica(data, fs):
+	eixo_x = [i/fs for i in range(len(data))]
+	eixo_y_e = data[:,0]/np.max(np.abs(data))
+	eixo_y_d = data[:,0]/np.max(np.abs(data))
+
+	plt.figure(1)
+	plt.subplot(211)
+	plt.plot(eixo_x, eixo_y_e)
+	plt.xlabel('Tempo [s]')
+	plt.ylabel('Amplitude [-1:1]')
+	plt.title('Canal Esquerdo')
+	plt.subplots_adjust(hspace=0.5)
+	
+	plt.subplot(212)
+	plt.plot(eixo_x, eixo_y_d)
+	plt.xlabel('Tempo [s]')
+	plt.ylabel('Amplitude [-1:1]')
+	plt.title('Canal Direito')
+	plt.subplots_adjust(hspace=0.5)
+
+	plt.show()
+
+def main():
+	filename = './files/drumloop.wav'
+
+	fs, data = wavfile.read(filename)
+
+	visualizacaoGrafica(data, fs)
+
+if __name__ == "__main__":
+	main()
